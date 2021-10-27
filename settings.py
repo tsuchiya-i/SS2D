49386486e133dd02bb546b7a9207908d932a8d71
tsuchiya-i <41734571+tsuchiya-i@users.noreply.github.com>
2021-10-27 16:30:06 +0900

    image window complete

--- settings.py
+++ settings.py
@@ -40,9 +40,8 @@ class settings_gui(Tk):
         super().__init__()
     
         #画像設置
-        self.canvas_width = 300
+        self.canvas_width = 400
         self.canvas_height = 200
-        self.geometry("800x400")
 
         self.map_canvas = Canvas(
             self,
@@ -50,27 +49,37 @@ class settings_gui(Tk):
             height=self.canvas_height,
             bg="gray"
         )
-        self.map_canvas.grid(rowspan=10, row=0, column=0, sticky=W+N)
+        self.map_canvas.grid(rowspan=10, row=0, column=0, sticky=W)
     
         # Frame1の作成
-        self.frame1 = ttk.Frame(padding=10)
-        self.frame1.grid(row=10, column=0, sticky=E)
+        self.frame1 = ttk.Frame(padding=5)
+        self.frame1.grid(row=11, column=0, sticky=W+E)
     
         # 「ファイル参照」ラベルの作成
-        self.IFileLabel = ttk.Label(self.frame1, text="file:", padding=(5, 2))
+        self.IFileLabel = ttk.Label(self.frame1, text="file:", padding=(3, 2))
         self.IFileLabel.pack(side=LEFT)
     
         # 「ファイル参照」エントリーの作成
         self.entry2 = StringVar()
-        self.IFileEntry = ttk.Entry(self.frame1, textvariable=self.entry2, width=30)
+        self.IFileEntry = ttk.Entry(self.frame1, textvariable=self.entry2, width=33)
         self.IFileEntry.pack(side=LEFT)
     
         # 「ファイル参照」ボタンの作成
-        self.IFileButton = ttk.Button(self.frame1, text="参照", command=self.filedialog_clicked)
-        self.IFileButton.pack(side=LEFT)
+        self.IFileButton = ttk.Button(self.frame1,
+                text="参照",
+                command=self.filedialog_clicked)
+        self.IFileButton.pack(side=RIGHT)
 
         self.map_canvas_obj= None
+        self.map_canvas.create_text(self.canvas_width//2, self.canvas_height//2, text="--no map--", font=("", "30", "bold"))
     
+        # Frame2の作成
+        self.frame2 = ttk.Frame(padding=5)
+        self.frame2.grid(row=12, column=0, sticky=W)
+        # 「ファイル参照」ラベルの作成
+        self.IFileLabel = ttk.Label(self.frame2, text="waypoints:")
+        self.IFileLabel.pack(side=LEFT)
+
         """
         # Frame3の作成
         frame3 = ttk.Frame(root, padding=10)
@@ -98,40 +107,24 @@ class settings_gui(Tk):
                 title = "open map image",
                 filetypes = fTyp,
                 initialdir = "./maps/")
-        self.entry2.set(file_path)
-    
-        #print(filename)
-        #print(type(filename))
-        #IFileButton["text"] = "aaa"
-    
-        self.img = Image.open(open(file_path, 'rb'))
-        self.img = self.img.resize((self.canvas_width // 2, self.canvas_height // 2))
-        #self.img.show()
-        self.map_image = ImageTk.PhotoImage(self.img)
-        
-        if self.map_canvas_obj is not None:
-                self.map_canvas.delete(self.map_canvas_obj)
-        self.map_canvas_obj = self.map_canvas.create_image(self.canvas_width/2, self.canvas_height/2, image=self.map_image)
-    
-    # 実行ボタン押下時の実行関数
-    def conductMain(self):
-        text = ""
-    
-        dirPath = self.entry1.get()
-        filePath = self.entry2.get()
-        if dirPath:
-            text += "フォルダパス：" + dirPath + "\n"
-        if filePath:
-            text += "ファイルパス：" + filePath
-    
-        if text:
-            messagebox.showinfo("info", text)
-        else:
-            messagebox.showerror("error", "パスの指定がありません。")
 
+        if len(file_path):
+            self.entry2.set(file_path)
+
+            self.img = Image.open(open(file_path, 'rb'))
+            self.img = self.img.resize((self.canvas_width, self.canvas_height))
+            self.map_image = ImageTk.PhotoImage(self.img)
+            
+            if self.map_canvas_obj is not None:
+                    self.map_canvas.delete(self.map_canvas_obj)
+            self.map_canvas_obj = self.map_canvas.create_image(
+                    self.canvas_width/2,
+                    self.canvas_height/2,
+                    image=self.map_image)
+    
 
 if __name__ == "__main__":
     gui = settings_gui()
     # rootの作成
-    #gui.title("window name")
+    gui.title("SS2D SETTING GUI")
     gui.mainloop()
