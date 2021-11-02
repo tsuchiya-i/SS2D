3606823c02c32f53b988dc51b1d3bd033474cb67
tsuchiya-i <41734571+tsuchiya-i@users.noreply.github.com>
2021-11-02 20:37:55 +0900

    development(next waypoint disply)

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ class config_data():
         self.map_data = None
         self.map_height = 0
         self.map_width = 0
-        self.waypoints = []
+        self.nwaypoints = []
         self.human_waypoints = []
         self.human_spawn_points = []
         self.robot_start_points = []
@@ -41,6 +41,8 @@ class settings_gui(Tk):
     def __init__(self):
         super().__init__()
         adjust_row = 10
+        self.nwaypoints = []
+        self.waypoints = []
         
         #画像設置
         self.canvas_width = 600
@@ -118,20 +120,25 @@ class settings_gui(Tk):
         self.frame_nwyp.grid(row=adjust_row, column=0, sticky=W+E)
         self.label_nwyp = ttk.Label(self.frame_nwyp, text="new waypoints name:")
         self.label_nwyp.pack(side=LEFT)
-        self.e_nwyp = StringVar()
-        self.entry_nwyp = ttk.Entry(self.frame_nwyp,
-                textvariable=self.e_nwyp,
-                width=50-len(self.label_nwyp.cget("text")))
-        self.entry_nwyp.pack(side=LEFT)
         self.button_save = ttk.Button(self.frame_nwyp,
                 text="save",
                 command=self.save)
         self.button_save['state'] = DISABLED
         self.button_save.pack(padx = 3,side=RIGHT)
+        self.button_back = ttk.Button(self.frame_nwyp,
+                text="back",
+                command=self.back)
+        self.button_back['state'] = DISABLED
+        self.button_back.pack(padx = 3,side=RIGHT)
         self.button_create = ttk.Button(self.frame_nwyp,
                 text="create",
                 command=self.create)
-        self.button_create.pack(padx = 3,side=RIGHT)
+        self.button_create.pack(padx = 5,side=RIGHT)
+        self.e_nwyp = StringVar()
+        self.entry_nwyp = ttk.Entry(self.frame_nwyp,
+                textvariable=self.e_nwyp,
+                width=41-len(self.label_nwyp.cget("text")))
+        self.entry_nwyp.pack(side=RIGHT)
 
         # Frame display text
         adjust_row += 1
@@ -139,14 +146,14 @@ class settings_gui(Tk):
         separator.grid(row=adjust_row, column=0, sticky="ew")
         adjust_row += 1
 
-        style = ttk.Style()
-        style.configure("console.TFrame", background="white")#, font=20, anchor="w")
-        self.frame_disp = ttk.Frame(padding=5)#,style="console.TFrame")
+        self.frame_disp = ttk.Frame(padding=5)
         self.frame_disp.grid(row=adjust_row, column=0, sticky=W+E)
-        self.label_disp = ScrolledText(self.frame_disp, font=("", 15), height=4,width=50)
-        self.label_disp.pack()
-        #self.label_disp = ttk.Label(self.frame_disp, text="_", background="white")
-        #self.label_disp.pack(side=LEFT)
+        self.scr_disp = ScrolledText(self.frame_disp, font=("",15), height=4,width=45)
+        self.scr_disp.pack()
+        self.insert_disp_text("> ")
+
+        #self.scr_disp = ttk.Label(self.frame_disp, text="_", background="white")
+        #self.scr_disp.pack(side=LEFT)
 
         # キャンセルボタンの設置
         #button_wayp = ttk.Button(frame_nwyp, text=("閉じる"), command=quit)
@@ -179,6 +186,16 @@ class settings_gui(Tk):
                     image=self.map_image)
 
     def waypointsfile_dialog(self):
+        if self.width == 0:
+            messagebox.showwarning('Error', "Load map image.")
+            return False
+        try:
+            reso = float(self.entry_reso_obj.get())
+            if reso <= 0:
+               raise Exception
+        except:
+            messagebox.showwarning('Error', "correct resolution value.")
+            return False
         format_name = ".bin"
         dir_name = "maps/waypoints/"
         fTyp = [("waypoints binary file",format_name)]
@@ -188,6 +205,10 @@ class settings_gui(Tk):
                 initialdir = "./"+dir_name)
         if len(file_path):
             self.entry_wayp.set(file_path)
+            with open(file_path, mode='rb') as f:
+                self.waypoints = pickle.load(f)
+            self.oval_remove(self.waypoints)
+            self.oval_draw(reso,self.waypoints)
 
     def yamlfile_dialog(self):
         format_name = ".yaml"
@@ -202,6 +223,7 @@ class settings_gui(Tk):
             with open(file_path, 'r') as yml:
                 map_yaml = yaml.load(yml,Loader=yaml.SafeLoader)
             if 'resolution' in map_yaml:
+                self.entry_reso_obj.delete(0, END)
                 self.entry_reso_obj.insert(END,map_yaml['resolution'])
             else:
                 messagebox.showwarning('Error', "This yaml has no resolution value.")
@@ -211,8 +233,10 @@ class settings_gui(Tk):
         click_x = event.x*self.width/self.canvas_width*reso
         click_y = (self.canvas_height-event.y)*self.height/self.canvas_height*reso
         r = 2
-        self.map_canvas.create_oval(event.x-r, event.y-r, event.x+r, event.y+r)
-        self.waypoints.append([click_x,click_y])
+        self.map_canvas.create_oval(event.x-r, event.y-r, event.x+r, event.y+r,
+                fill="cyan", tag="new_wyp"+str(len(self.nwaypoints)))
+        self.nwaypoints.append([click_x,click_y])
+        self.insert_disp_text(str([round(click_x,3),round(click_y,3)])+"\n")
 
     def save(self):
         if self.entry_nwyp.get() == "":
@@ -227,12 +251,15 @@ class settings_gui(Tk):
                 if yn == "no":
                     return False
         self.button_save["state"] = DISABLED
+        self.button_create["state"] = NORMAL
+        self.button_back["state"] = DISABLED
         self.entry_reso_obj.configure(state=NORMAL)
-        self.label_disp['text'] = ""
+        self.insert_disp_text("   Saved.\n> ")
         self.map_canvas.unbind('<Button-1>')
-        self.waypoints = np.array(self.waypoints)
         with open("./maps/waypoints/"+file_name+'.bin', mode='wb') as f:
-            pickle.dump(self.waypoints, f)
+            pickle.dump(self.nwaypoints, f)
+        self.oval_remove(self.nwaypoints)
+        self.nwaypoints = []
 
     def create(self):
         if self.width == 0:
@@ -245,13 +272,55 @@ class settings_gui(Tk):
         except:
             messagebox.showwarning('Error', "correct resolution value.")
             return False
-        self.label_disp['text'] = "click waypoints"
-        self.label_disp['foreground'] = "blue"
+        self.insert_disp_text("click points\n")
         self.button_save["state"] = NORMAL
+        self.button_create["state"] = DISABLED
+        self.button_back["state"] = NORMAL
         self.entry_reso_obj.configure(state=DISABLED)
         self.map_canvas.bind('<Button-1>', self.click_canvas)
-        self.waypoints = []
+        self.oval_remove(self.waypoints)
 
+    def back(self):
+        if len(self.nwaypoints) == 0:
+            self.button_save["state"] = DISABLED
+            self.button_create["state"] = NORMAL
+            self.button_back["state"] = DISABLED
+            self.map_canvas.unbind('<Button-1>')
+            self.insert_disp_text("  Canceled.\n> ")
+            return True
+        self.nwaypoints.pop()
+        self.delete_disp_text('end -2lines linestart', END)
+        self.insert_disp_text("\n")
+        self.map_canvas.delete("new_wyp"+str(len(self.nwaypoints)))
+
+    def delete_disp_text(self,start,end):
+        self.scr_disp["state"] = NORMAL
+        self.scr_disp.delete(start,end)
+        self.scr_disp.see("end")
+        self.scr_disp["state"] = DISABLED
+    def insert_disp_text(self,text):
+        self.scr_disp["state"] = NORMAL
+        self.scr_disp.insert("end",text)
+        self.scr_disp.see("end")
+        self.scr_disp["state"] = DISABLED
+    def oval_remove(self,points):
+        tag_name = self.tag_name_select(points)
+        for i in range(len(points)):
+            self.map_canvas.delete(tag_name+str(i))
+    def oval_draw(self,reso,points):
+        tag_name = self.tag_name_select(points)
+        r = 2
+        for i, xy in enumerate(self.waypoints):
+            x = xy[0]/(self.width/self.canvas_width*reso)
+            y = self.canvas_height - xy[1]/(self.height/self.canvas_height*reso)
+            self.map_canvas.create_oval(x-r, y-r, x+r, y+r,
+                    fill="cyan", tag=tag_name+str(i))
+    def tag_name_select(self,points):
+        if self.nwaypoints == points:
+            tag_name = "new_wyp"
+        elif self.waypoints == points:
+            tag_name = "wyp"
+        return tag_name
 
 if __name__ == "__main__":
     gui = settings_gui()
