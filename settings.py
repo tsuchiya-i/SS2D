057f87fefd746cc8f0359963bb441684b218caa3
tsuchiya-i <41734571+tsuchiya-i@users.noreply.github.com>
2021-11-10 10:20:43 +0900

    config

--- settings.py
+++ settings.py
@@ -27,14 +27,29 @@ def image_thresh(file_name):
 
 class settings_gui(Tk):
     def __init__(self):
+        if os.path.exists("./ss2d/envs/config.bin"):
+            with open("./ss2d/envs/config.bin", mode='rb') as f:
+                self.config = pickle.load(f)
+        else:
+            self.config = configClass()
+
         super().__init__()
         adjust_row = 10
         self.nwaypoints = []
         #save parameter
-        self.waypoints = []
-        self.goal_points = []
-        self.human_points = []
-        self.reso = 0
+        self.waypoints = self.config.start_points
+        self.goal_points = self.config.goal_points
+        self.human_points = self.config.human_points
+        self.reso = self.config.reso
+        if self.waypoints == self.goal_points:
+            self.init_goal_option = 1
+        else:
+            self.init_goal_option = 0
+        if self.waypoints == self.human_points:
+            self.init_human_option = 1
+        else:
+            self.init_human_option = 0
+
         
         #image set
         self.canvas_width = 600
@@ -50,15 +65,32 @@ class settings_gui(Tk):
         self.map_canvas.grid(rowspan=adjust_row, row=1, column=0, sticky=W)
         self.map_canvas.unbind('<Button-1>')
         self.map_canvas_obj= None
-        self.map_canvas.create_text(self.canvas_width//2, self.canvas_height//2,
-                text="--no map--", font=("", "30", "bold"))
+
+        self.cv_thresh_image = self.config.thresh_map
+        if self.cv_thresh_image is not None:
+            self.img = Image.fromarray(self.cv_thresh_image)
+            self.width, self.height = self.img.size
+            self.img = self.img.resize((self.canvas_width, self.canvas_height))
+            self.map_image = ImageTk.PhotoImage(self.img)
+            self.map_canvas_obj = self.map_canvas.create_image(
+                    self.canvas_width/2,
+                    self.canvas_height/2,
+                    image=self.map_image)
+            self.oval_remove("wyp")
+        else:
+            self.map_canvas.create_text(self.canvas_width//2, self.canvas_height//2,
+                    text="--no map--", font=("", "30", "bold"))
+        self.oval_draw(self.reso,"wyp")
+        self.oval_draw(self.reso,"goal_wyp","red")
+        self.oval_draw(self.reso,"human_wyp","orange")
+
     
         # Frame input map reso
         self.frame_reso = ttk.Frame(padding=5)
         self.frame_reso.grid(row=0, column=0, sticky=W+E)
         self.label_reso_str = ttk.Label(self.frame_reso, text="map resolution:")
         self.label_reso_str.pack(side=LEFT)
-        self.entry_reso = DoubleVar(value=0.05)
+        self.entry_reso = DoubleVar(value=self.config.reso)
         self.entry_reso_obj = ttk.Entry(self.frame_reso,
                 textvariable=self.entry_reso,
                 width=8)
@@ -180,7 +212,7 @@ class settings_gui(Tk):
         self.frame_world.grid(row=R_adjust_row, column=1, sticky=W)
         self.label_dt = ttk.Label(self.frame_world, text=" dt: ")
         self.label_dt.pack(side=LEFT)
-        self.entry_dt = DoubleVar(value=0.1)
+        self.entry_dt = DoubleVar(value=self.config.world_dt)
         self.entry_dt_obj = ttk.Entry(self.frame_world,
                 textvariable=self.entry_dt,
                 width=10)
@@ -192,7 +224,7 @@ class settings_gui(Tk):
         self.frame_goal = ttk.Labelframe(padding=5, width=width, height=75, text="Goal point option")
         self.frame_goal.propagate(False) 
         self.frame_goal.grid(row=R_adjust_row, column=1, sticky=W)
-        self.goal_option = IntVar()
+        self.goal_option = IntVar(value=self.init_goal_option)
         rb1 = Radiobutton(self.frame_goal, text='random from goal points',
                 value=0, variable=self.goal_option)
         rb1.pack(anchor=W)
@@ -206,7 +238,7 @@ class settings_gui(Tk):
         self.frame_robot.grid(row=R_adjust_row, column=1, sticky=W+E)
         self.label_radius = ttk.Label(self.frame_robot, text=" Robot radius: ")
         self.label_radius.grid(row=0, column=0, sticky=W)
-        self.entry_radius = DoubleVar(value=0.3)
+        self.entry_radius = DoubleVar(value=self.config.robot_r)
         self.entry_radius_obj = ttk.Entry(self.frame_robot,
                 textvariable=self.entry_radius,
                 width=10)
@@ -217,7 +249,7 @@ class settings_gui(Tk):
         self.label_.grid(row=1, column=0, sticky=W)
         self.label_ = ttk.Label(self.frame_robot, text="  max: ")
         self.label_.grid(row=2, column=0, sticky=E)
-        self.entry_lmax = DoubleVar(value=10.0)
+        self.entry_lmax = DoubleVar(value=self.config.lidar_max)
         self.entry_lmax_obj = ttk.Entry(self.frame_robot,
                 textvariable=self.entry_lmax,
                 width=10)
@@ -226,7 +258,7 @@ class settings_gui(Tk):
         self.label_.grid(row=2, column=2, sticky=W)
         self.label_ = ttk.Label(self.frame_robot, text="  min: ")
         self.label_.grid(row=3, column=0, sticky=E)
-        self.entry_lmin = DoubleVar(value=0.2)
+        self.entry_lmin = DoubleVar(value=self.config.lidar_min)
         self.entry_lmin_obj = ttk.Entry(self.frame_robot,
                 textvariable=self.entry_lmin,
                 width=10)
@@ -235,7 +267,7 @@ class settings_gui(Tk):
         self.label_.grid(row=3, column=2, sticky=W)
         self.label_ = ttk.Label(self.frame_robot, text="Viewing angle: ")
         self.label_.grid(row=4, column=0, sticky=E)
-        self.entry_la = DoubleVar(value=90)
+        self.entry_la = DoubleVar(value=self.config.lidar_angle)
         self.entry_la_obj = ttk.Entry(self.frame_robot,
                 textvariable=self.entry_la,
                 width=10)
@@ -244,7 +276,7 @@ class settings_gui(Tk):
         self.label_.grid(row=4, column=2, sticky=W)
         self.label_ = ttk.Label(self.frame_robot, text="Angle reso: ")
         self.label_.grid(row=5, column=0, sticky=E)
-        self.entry_lar = DoubleVar(value=10)
+        self.entry_lar = DoubleVar(value=self.config.lidar_reso)
         self.entry_lar_obj = ttk.Entry(self.frame_robot,
                 textvariable=self.entry_lar,
                 width=10)
@@ -258,7 +290,7 @@ class settings_gui(Tk):
         self.frame_human.grid(row=R_adjust_row, column=1, sticky=W+E)
         self.label_ = ttk.Label(self.frame_human, text="Number of people: ")
         self.label_.grid(row=1, column=0, sticky=W)
-        self.entry_hn = IntVar(value=0)
+        self.entry_hn = IntVar(value=self.config.human_n)
         self.entry_hn_obj = ttk.Entry(self.frame_human,
                 textvariable=self.entry_hn,
                 width=10)
@@ -267,7 +299,7 @@ class settings_gui(Tk):
         self.label_.grid(row=1, column=2, sticky=E)
         self.label_ = ttk.Label(self.frame_human, text="Human spawn point: ")
         self.label_.grid(row=2, column=0, sticky=W)
-        self.human_option = IntVar()
+        self.human_option = IntVar(value=self.init_human_option)
         rb1 = Radiobutton(self.frame_human, text='random from human spawn points',
                 value='0', variable=self.human_option)
         rb1.grid(columnspan=3, row=3, column=0, sticky=W)
@@ -279,7 +311,7 @@ class settings_gui(Tk):
         self.frame_observe = ttk.Labelframe(padding=5, width=width, height=100, text="Observe option")
         self.frame_observe.propagate(False)
         self.frame_observe.grid(row=R_adjust_row, column=1, sticky=W+E)
-        self.detect_TF = BooleanVar()
+        self.detect_TF = BooleanVar(value=self.config.human_detect)
         check = Checkbutton(self.frame_observe, text="Activate Human Detection",
                 variable=self.detect_TF)
         check.grid(row=2, column=0, sticky=W)
@@ -289,11 +321,11 @@ class settings_gui(Tk):
         self.frame_sim = ttk.Labelframe(padding=5, width=width, height=200, text="Simulator option")
         self.frame_sim.propagate(False)
         self.frame_sim.grid(rowspan=8, row=R_adjust_row, column=1, sticky=W+E+N)
-        self.wall_TF = BooleanVar()
+        self.wall_TF = BooleanVar(value=self.config.wall_render)
         check = Checkbutton(self.frame_sim, text="Activate wall Rendering",
                 variable=self.wall_TF)
         check.grid(row=1, column=0, sticky=W)
-        self.output_TF = BooleanVar()
+        self.output_TF = BooleanVar(value=self.config.console_output)
         check = Checkbutton(self.frame_sim, text="Console output(collision & goal)",
                 variable=self.output_TF)
         check.grid(row=2, column=0, sticky=W)
@@ -354,7 +386,9 @@ class settings_gui(Tk):
                     self.canvas_width/2,
                     self.canvas_height/2,
                     image=self.map_image)
-            self.oval_remove(self.waypoints)
+            self.oval_remove("wyp")
+            self.oval_remove("goal_wyp")
+            self.oval_remove("human_wyp")
             self.waypoints = []
 
     def waypointsfile_dialog(self):
@@ -369,10 +403,10 @@ class settings_gui(Tk):
                     initialdir = "./"+dir_name)
             if len(file_path):
                 self.entry_wayp.set(file_path)
-                self.oval_remove(self.waypoints)
+                self.oval_remove("wyp")
                 with open(file_path, mode='rb') as f:
                     self.waypoints = pickle.load(f)
-                self.oval_draw(self.reso,self.waypoints)
+                self.oval_draw(self.reso,"wyp")
 
     def goal_pointsfile_dialog(self):
         if self.map_reso_check():
@@ -386,10 +420,10 @@ class settings_gui(Tk):
                     initialdir = "./"+dir_name)
             if len(file_path):
                 self.entry_gp.set(file_path)
-                self.oval_remove(self.goal_points)
+                self.oval_remove("goal_wyp")
                 with open(file_path, mode='rb') as f:
                     self.goal_points = pickle.load(f)
-                self.oval_draw(self.reso,self.goal_points,"red")
+                self.oval_draw(self.reso,"goal_wyp","red")
 
     def human_pointsfile_dialog(self):
         if self.map_reso_check():
@@ -403,10 +437,10 @@ class settings_gui(Tk):
                     initialdir = "./"+dir_name)
             if len(file_path):
                 self.entry_hp.set(file_path)
-                self.oval_remove(self.human_points)
+                self.oval_remove("human_wyp")
                 with open(file_path, mode='rb') as f:
                     self.human_points = pickle.load(f)
-                self.oval_draw(self.reso,self.human_points,"orange")
+                self.oval_draw(self.reso,"human_wyp","orange")
 
     def yamlfile_dialog(self):
         format_name = ".yaml"
@@ -459,11 +493,11 @@ class settings_gui(Tk):
             pickle.dump(self.nwaypoints, f)
         self.insert_disp_text("   "+file_name+".bin Saved.\n("+str(len(self.nwaypoints))+" points)\n> ")
         self.entry_nwyp_obj.delete(0,END)
-        self.oval_remove(self.nwaypoints)
+        self.oval_remove("new_wyp")
         self.nwaypoints = []
-        self.oval_draw(self.reso,self.waypoints)
-        self.oval_draw(self.reso,self.goal_points)
-        self.oval_draw(self.reso,self.human_points)
+        self.oval_draw(self.reso,"wyp")
+        self.oval_draw(self.reso,"goal_wyp","red")
+        self.oval_draw(self.reso,"human_wyp","orange")
 
     def create(self):
         if self.map_reso_check():
@@ -473,9 +507,9 @@ class settings_gui(Tk):
             self.button_back["state"] = NORMAL
             self.entry_reso_obj.configure(state=DISABLED)
             self.map_canvas.bind('<Button-1>', self.click_canvas)
-            self.oval_remove(self.waypoints)
-            self.oval_remove(self.goal_points)
-            self.oval_remove(self.human_points)
+            self.oval_remove("wyp")
+            self.oval_remove("goal_wyp")
+            self.oval_remove("human_wyp")
 
     def back(self):
         if len(self.nwaypoints) == 0:
@@ -485,9 +519,9 @@ class settings_gui(Tk):
             self.map_canvas.unbind('<Button-1>')
             self.insert_disp_text("  Canceled.\n> ")
             self.entry_reso_obj.configure(state=NORMAL)
-            self.oval_draw(self.reso,self.waypoints)
-            self.oval_draw(self.reso,self.goal_points,"red")
-            self.oval_draw(self.reso,self.human_points,"orange")
+            self.oval_draw(self.reso,"wyp")
+            self.oval_draw(self.reso,"goal_wyp","red")
+            self.oval_draw(self.reso,"human_wyp","orange")
             return True
         self.nwaypoints.pop()
         self.delete_disp_text('end -2lines linestart', END)
@@ -562,29 +596,28 @@ class settings_gui(Tk):
         self.scr_disp.insert("end",text)
         self.scr_disp.see("end")
         self.scr_disp["state"] = DISABLED
-    def oval_remove(self,points):
-        tag_name = self.tag_name_select(points)
+    def oval_remove(self,tag_name):
+        points = self.tag_name_select(tag_name)
         for i in range(len(points)):
             self.map_canvas.delete(tag_name+str(i))
-    def oval_draw(self,reso,points,color="cyan"):
-        tag_name = self.tag_name_select(points)
+    def oval_draw(self,reso,tag_name,color="cyan"):
+        points = self.tag_name_select(tag_name)
         r = 2
         for i, xy in enumerate(points):
             x = xy[0]/(self.width/self.canvas_width*reso)
             y = self.canvas_height - xy[1]/(self.height/self.canvas_height*reso)
             self.map_canvas.create_oval(x-r, y-r, x+r, y+r,
                     fill=color, tag=tag_name+str(i))
-    def tag_name_select(self,points):
-        if self.nwaypoints == points:
-            tag_name = "new_wyp"
-        elif self.waypoints == points:
-            tag_name = "wyp"
-        elif self.goal_points == points:
-            tag_name = "goal_wyp"
-        elif self.human_points == points:
-            tag_name = "human_wyp"
-
-        return tag_name
+    def tag_name_select(self,tag_name):
+        if tag_name == "new_wyp":
+            points = self.nwaypoints
+        if tag_name == "wyp":
+            points = self.waypoints
+        if tag_name == "goal_wyp":
+            points = self.goal_points
+        if tag_name == "human_wyp":
+            points = self.human_points
+        return points
 
     def map_reso_check(self):
         if self.width == 0:
