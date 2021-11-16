913ca4657e3d4e47145d08bcbf852e74cae6fb50
tsuchiya-i <41734571+tsuchiya-i@users.noreply.github.com>
2021-11-16 17:54:41 +0900

    change param

--- example/train_ddpg.py
+++ example/train_ddpg.py
@@ -37,7 +37,7 @@ def main():
     actor.add(Activation('relu'))
     actor.add(Dense(nb_actions))
     actor.add(Activation('tanh'))
-    print(actor.summary())
+    #print(actor.summary())
     
     action_input = Input(shape=(nb_actions,), name='action_input')
     observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
@@ -54,17 +54,25 @@ def main():
     x = Dense(1)(x)
     x = Activation('linear')(x)
     critic = Model(inputs=[action_input, observation_input], outputs=x)
+    print("##############################")
     print(critic.summary())
+    print("##############################")
+    #from tensorflow.keras.utils import plot_model
+    #plot_model(
+    #    critic,
+    #    show_shapes=True,
+    #)
+
     
     # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
     memory = SequentialMemory(limit=100000, window_length=1)
-    random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.3)
+    random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0.2, sigma=.3)
     
     agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
-                      memory=memory, nb_steps_warmup_critic=10000, nb_steps_warmup_actor=10000,
+                      memory=memory, nb_steps_warmup_critic=100, nb_steps_warmup_actor=100,
                       random_process=random_process, gamma=.99, target_model_update=1e-3)
     agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
-    tensorboard_callback = TensorBoard(log_dir="logs", histogram_freq=1)
+    tensorboard_callback = TensorBoard(log_dir="./logs")
     
     if os.path.exists(check_weight_path):
         agent.load_weights(save_weight_path)
@@ -77,10 +85,10 @@ def main():
     # Okay, now it's time to learn something! We visualize the training here for show, but this
     # slows down training quite a lot. You can always safely abort the training prematurely using
     # Ctrl + C.
-    nb_steps_ = 5000000000
+    nb_steps_ = 100000
     nb_max_episode_steps_ = 1500
     
-    train_history = agent.fit(env, nb_steps=nb_steps_, visualize=True, verbose=1, nb_max_episode_steps=nb_max_episode_steps_)
+    train_history = agent.fit(env, nb_steps=nb_steps_, visualize=True, verbose=1, nb_max_episode_steps=nb_max_episode_steps_, callbacks=[tensorboard_callback])
     
     # After training is done, we save the final weights.
     agent.save_weights(save_weight_path, overwrite=True)
