3ffbcefa1153d4b839cd5b4103c09fd9682ad913
tsuchiya-i <41734571+tsuchiya-i@users.noreply.github.com>
2021-11-24 14:51:45 +0900

    Change from forwaord to predict, add argument

--- example/test_ddpg.py
+++ example/test_ddpg.py
@@ -8,13 +8,14 @@ import numpy as np
 
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Activation, Flatten, Input, Concatenate
-from tensorflow.keras.optimizers import Adam
-from rl.agents import DDPGAgent
-from rl.memory import SequentialMemory
-from rl.random import OrnsteinUhlenbeckProcess
 
-weight_path = "./trained_weight/ddpg_weights.h5f"
 check_weight_path = "./trained_weight/ddpg_weights_actor.h5f.index"
+weight_path = "./trained_weight/ddpg_weights_actor.h5f"
+
+args = sys.argv
+if len(args)>1:
+    check_weight_path = args[1]
+    weight_path = args[1][:-6]
 
 # Get the environment and extract the number of actions.
 env = gym.make('ss2d-v0')
@@ -36,32 +37,8 @@ actor.add(Dense(nb_actions))
 actor.add(Activation('tanh'))
 print(actor.summary())
 
-action_input = Input(shape=(nb_actions,), name='action_input')
-observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
-flattened_observation = Flatten()(observation_input)
-x = Concatenate()([action_input, flattened_observation])
-x = Dense(256)(x)
-x = Activation('relu')(x)
-x = Dense(128)(x)
-x = Activation('relu')(x)
-x = Dense(64)(x)
-x = Activation('relu')(x)
-x = Dense(1)(x)
-x = Activation('linear')(x)
-critic = Model(inputs=[action_input, observation_input], outputs=x)
-print(critic.summary())
-
-# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-memory = SequentialMemory(limit=100000, window_length=1)
-random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.3)
-
-agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
-                  memory=memory, nb_steps_warmup_critic=10, nb_steps_warmup_actor=10,
-                  random_process=random_process, gamma=.99, target_model_update=1e-3)
-agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
-
 if os.path.exists(check_weight_path):
-    agent.load_weights(weight_path)
+    actor.load_weights(weight_path)
     print("find weights-file")
 else:
     print("not found weights-file")
@@ -69,8 +46,8 @@ else:
 observation = env.reset()
 while True:
     env.render()
-    action = agent.forward(observation)
-    observation, reward, done,  GoalOrNot = env.step(action)
-    if done:
+    action = actor.predict(observation.reshape(1,1,len(observation)))
+    observation, reward, done,  GoalOrNot = env.step(action.reshape(2))
+    if done or env.step_count>150:
         env.reset()
 
